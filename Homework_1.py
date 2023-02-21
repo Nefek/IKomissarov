50358e907ad80e20967a1c9c3beee2057a2d5b83
Nefek <117991005+Nefek@users.noreply.github.com>
2023-02-21 09:19:28 +0300

    Update Homework_1.py

--- Homework_1.py
+++ Homework_1.py
@@ -7,3 +7,35 @@ elif a < b:
 else:
     c = a
 print(c)
+
+a = input()
+b = input()
+if a == "красный":
+    if a == b:
+        print("красный")
+    elif b == "синий":
+        print ("фиолетовый")
+    elif b == "желтый":
+        print("оранжевый")
+    else:
+        print ("ошибка цвета")
+elif a == "синий":
+    if a == b:
+        print("синий")
+    elif b == "красный":
+        print("фиолетовый")
+    elif b == "желтый":
+        print("зеленый")
+    else:
+        print("ошибка цвета")
+elif a == "желтый":
+    if a == b:
+        print("желтый")
+    elif b == "красный":
+        print("оранжевый")
+    elif b == "синий":
+        print("зеленый")
+    else:
+        print("ошибка цвета")
+else:
+    print("ошибка цвета")
