7424b0dd2ff4495575f6ef1f98fc2b82442ab8da
Nefek <117991005+Nefek@users.noreply.github.com>
2023-04-11 10:03:13 +0300

    Update Classwork_7.py

--- Classwork_7.py
+++ Classwork_7.py
@@ -1,27 +1,31 @@
 #___№1___
-for i in range(2, 101):
-    if i ** 0.5 < 3.14:
-        print(f"{i ** 0.5} ({i})")
+a, n = 1, 3.14  
+while a ** 0.5 <= n:
+    if a ** 0.5 < 3.14:
+        print(f"{a ** 0.5} ({a})")
+    a += 1
 #___№2___ 
 a, b = int(input()), int(input())     
-for i in range(a + 1, b):
-    print(i)
+while a < b:
+    a += 1
+    print(a)
 #___№3___ 
 a, b = int(input()), int(input())     
-for i in range(b - 1, a, -1):
-    print(i)
+while a < b:
+    b -= 1
+    print(b)
 #___№4___
-n = int(input())
-for i in range(1, 1000000):
-    if i * 3 >= n:
-        print(i, i * 3)
-        break
+n, a = int(input()), 1
+while a * 3 < n:
+    a += 1
+print(a, a * 3)
 #___№5___
-n = int(input())
-for i in range(1, 1000000):
-    if i * 3 <= n:
-        print(i, i * 3)
-        break
-
+n, a = int(input()), 1
+while a * 3 < n:
+    a += 1
+if a * 3 > n:
+    print(a - 1)
+else:
+    print(a)
 
 
