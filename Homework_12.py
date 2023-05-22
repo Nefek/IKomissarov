a252a17ed40d6a76cebd81fc03a06c26cf80e443
Nefek <117991005+Nefek@users.noreply.github.com>
2023-05-22 04:00:37 +0300

    3 загадки

---
+++ Homework_12.py
@@ -0,0 +1,29 @@
+print("какое число от 1 до 10 я загнадал?")
+a = int(input())
+while a != 7:
+    print("не то число")
+    a = int(input())
+print("верно")
+
+
+print("какого числа у меня день рождения?")
+b = int(input())
+while b != 18:
+    print("не то число")
+    b = int(input())
+print("верно")
+
+
+print("какая столица у Нидерландов?")
+c = input()
+while c != "Амстердам" and c != "амстердам":
+    print("другой город")
+    c = input()
+print("верно")
+
+
+
+
+
+
+
